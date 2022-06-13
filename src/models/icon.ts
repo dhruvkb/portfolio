9a111dd8b67bbb11d14f095d790a8a1dc58d9c0c
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-13 10:10:22 +0400

    Clean up icon assets and define icon models

--- src/models/icon.ts
+++ src/models/icon.ts
@@ -1,9 +1,23 @@
+/**
+ * Represents an SVG `path` tag.
+ */
 export interface Path {
   d: string
-  'fill-rule': 'nonzero' | 'evenodd'
+
+  // This affects fill and stroke if they are set to `'currentColor'`.
   color?: string
+
+  // `fill` defaults to `'currentColor'`. For unfilled paths, set to `'none'`.
+  fill?: string
+  'fill-rule'?: 'nonzero' | 'evenodd'
+
+  stroke?: string
+  'stroke-width'?: number
 }
 
+/**
+ * Represents an icon which, in its simplest form, is but a collection of paths.
+ */
 export interface Icon {
   paths: Path[]
 }
