07e17df36791518c758cdc5797ed311a01e7e265
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-13 08:40:50 +0400

    Drop Solarized in favour of Tailwind's built-in palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './index.html',
@@ -17,31 +21,29 @@ module.exports = {
       fontSize: {
         mono: ['0.875rem', '1.2'],
       },
-    },
-    colors: {
-      curr: 'currentColor',
-      tx: 'transparent',
-      white: '#ffffff',
-      black: '#000000',
-      sol: {
-        // Base colors
-        '03': '#002b36', // L* 15
-        '02': '#073642', // L* 20
-        '01': '#586e75', // L* 45
-        '00': '#657b83', // L* 50
-        0: '#839496', // L* 60
-        1: '#93a1a1', // L* 65
-        2: '#eee8d5', // L* 92 ~90
-        3: '#fdf6e3', // L* 97 ~95
-        // Accent colors
-        y: '#b58900', // yellow; Split comp
-        o: '#cb4b16', // orange; Complement
-        r: '#dc322f', // red; Triad
-        m: '#d33682', // magenta; Tetrad
-        v: '#6c71c4', // violet; Analogous
-        b: '#268bd2', // blue; Monotone
-        c: '#2aa198', // cyan; Analogous
-        g: '#859900', // green; Tetrad
+      colors: {
+        in: colors.inherit,
+        curr: colors.current,
+        tx: colors.transparent,
+
+        /*
+         Theme guide
+         ===========
+
+         Light theme:
+         - background:            white
+         - emphasised background: 200
+         - de-emphasised text:    500
+         - regular text:          700
+         - emphasised text:       900
+
+         Dark theme:
+         - background:            black
+         - emphasised background: 900
+         - de-emphasised text:    600
+         - regular text:          400
+         - emphasised text:       200
+         */
       },
     },
     fontFamily: {
