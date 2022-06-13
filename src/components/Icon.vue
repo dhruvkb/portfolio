af84bbcc8f34eb76df9f6e5aaff987fb8710827b
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-13 10:14:26 +0400

    Create a component for rendering icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<!--
+Renders an icon composed of specified paths as an SVG. This icon must be purely
+presentational as it is hidden from assistive technologies.
+-->
+
+<script setup lang="ts">
+  import { PropType } from 'vue'
+
+  import { Path } from '@/models/icon'
+
+  defineProps({
+    paths: {
+      type: Array as PropType<Path[]>,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <svg
+    class="h-4 w-4"
+    viewBox="0 0 24 24"
+    aria-hidden="true">
+    <path
+      v-for="(path, index) in paths"
+      :key="index"
+      v-bind="path"
+      :fill="path.fill ?? 'currentColor'" />
+  </svg>
+</template>
