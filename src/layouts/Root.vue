68c992eff9f522e2e52fe400b45a5ba87446748e
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-19 16:25:54 +0400

    Reduce the work of the section class

--- src/layouts/Root.vue
+++ src/layouts/Root.vue
@@ -7,19 +7,17 @@
 </script>
 
 <template>
-  <main class="flex h-full flex-col pts-8 pbs-8 lg:pts-12 lg:pbs-12">
-    <HelloSection class="section" />
-    <DeedsSection class="section" />
-    <ResumeSection class="section" />
-    <ContactSection class="section" />
-    <FooterSection class="section mt-auto" />
+  <main class="flex h-full flex-col">
+    <HelloSection class="section pb-8 pts-8 lg:pb-12 lg:pts-12" />
+    <DeedsSection class="section py-8 lg:py-12" />
+    <ResumeSection class="section py-8 lg:py-12" />
+    <ContactSection class="section py-8 lg:py-12" />
+    <FooterSection class="section pt-8 pbs-8 lg:pt-12 lg:pbs-12" />
   </main>
 </template>
 
 <style>
   .section {
-    @apply py-8 lg:py-12 first:pt-0 lg:first:pt-0 last:pb-0 lg:last:pb-0;
-    @apply border-b last:border-b-0;
-    @apply border-neutral-200 dark:border-neutral-900;
+    @apply border-b last:border-b-0 border-neutral-200 dark:border-neutral-900;
   }
 </style>
