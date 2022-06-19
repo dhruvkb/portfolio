d4ea91308e89d722e25df1b2192c6192891c009c
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-19 09:08:36 +0400

    Make the period cell screen-reader friendly

--- src/cells/PeriodCell.vue
+++ src/cells/PeriodCell.vue
@@ -14,24 +14,49 @@ is still active.
 
   const isActive = computed(() => !props.end)
   const description = computed(() => (isActive.value ? 'Active role' : 'Past role'))
+
+  const readableDate = (period: string[]) => {
+    const [shortMonth, yy] = period
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const monthNum = monthNames.indexOf(shortMonth)
+    const yearNum = parseInt(`20${yy}`, 10)
+    return new Date(yearNum, monthNum, 1).toLocaleString('default', { year: 'numeric', month: 'long' })
+  }
+
+  const descText = computed(() => {
+    let period
+    if (props.end) {
+      period = `${readableDate(props.start)} to ${readableDate(props.end)}`
+    } else {
+      period = `${readableDate(props.start)} to present`
+    }
+    return `${description.value}: ${period}`
+  })
 </script>
 
 <template>
-  <div class="flex flex-row items-center justify-center sm:justify-between">
-    <span class="hidden font-mono tracking-tighter md:inline">
-      <span>{{ start.join("'") }}</span>
+  <span class="flex flex-row items-center justify-center sm:justify-between">
+    <span class="sr-only">{{ descText }}</span>
+
+    <span
+      class="hidden font-mono tracking-tighter md:inline"
+      aria-hidden="true">
+      <span :title="readableDate(start)">{{ start.join("'") }}</span>
       <span class="m-1 text-neutral-400 dark:text-neutral-600">&ndash;</span>
-      <span v-if="!isActive">{{ end?.join("'") }}</span>
+      <span
+        v-if="end"
+        :title="readableDate(end)">{{ end.join("'") }}</span>
     </span>
 
-    <div
-      class="h-2 w-2"
-      :title="description">
-      <div
-        class="h-full w-full rounded-full border border-curr"
+    <span
+      class="block h-2 w-2"
+      :title="description"
+      aria-hidden="true">
+      <span
+        class="block h-full w-full rounded-full border border-curr"
         :class="{'text-red-500 bg-curr': isActive}"
         aria-hidden="true" />
       <span class="sr-only">{{ description }}</span>
-    </div>
-  </div>
+    </span>
+  </span>
 </template>
