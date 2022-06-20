4dce63d1536e23b138c4e296a8b7c9dc92540061
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-20 20:38:36 +0400

    Make a separate page from the hello and deeds sections

--- src/layouts/Root.vue
+++ src/layouts/Root.vue
@@ -8,8 +8,11 @@
 
 <template>
   <main class="flex h-full flex-col">
-    <HelloSection class="section pb-8 pts-8 lg:pb-12 lg:pts-12" />
-    <DeedsSection class="section py-8 lg:py-12" />
+    <div class=" flex min-h-screen flex-col justify-between gap-16 border-b border-neutral-200 pb-8 pts-8 dark:border-neutral-900 lg:gap-24 lg:pb-12 lg:pts-12">
+      <HelloSection />
+      <DeedsSection class="lg:self-end lg:text-right" />
+    </div>
+
     <ResumeSection class="section py-8 lg:py-12" />
     <ContactSection class="section py-8 lg:py-12" />
     <FooterSection class="section pt-8 pbs-8 lg:pt-12 lg:pbs-12" />
