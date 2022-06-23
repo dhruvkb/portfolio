e3c986ca1c685f0caa65da5b0c63b23b77f7c2ab
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-23 07:22:04 +0400

    Install Astro for SSG

---
+++ astro.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+
+import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  integrations: [vue()],
+})
