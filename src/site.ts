8359e615b678bd6b4612d03402c6e91fab26c4b5
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-30 14:54:45 +0400

    Add résumé URL to site-wide data

--- src/site.ts
+++ src/site.ts
@@ -2,4 +2,5 @@ export default {
   title: 'Dhruv Bhanushali',
   description: 'Hi! I am Dhruv Bhanushali, a software engineer who writes well-documented code, builds accessible interfaces and ships delightful apps.',
   author: 'Dhruv Bhanushali',
+  resumeUrl: 'https://raw.githubusercontent.com/dhruvkb/reschume/main/sample_data/dhruvkb.json',
 }
