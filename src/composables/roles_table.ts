b51a062e14bdb1523acdc0e271f738bd60556292
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-02 15:03:20 +0400

    Map role type enum to human-readable text

--- src/composables/roles_table.ts
+++ src/composables/roles_table.ts
@@ -51,12 +51,20 @@ export const useRoleTable = () => {
   ] as const
   type RoleData = RowData<typeof columns[number]['code']>
 
+  const roleTypes: Record<RoleType, string> = {
+    'foss-contributor': 'FOSS',
+    'part-timer': 'Part-timer',
+    'full-timer': 'Full-timer',
+    intern: 'Intern',
+    contractor: 'Contractor',
+  }
+
   const data = computed(() => roles.value.map((role): RoleData => ({
     isLast: role.isLast,
     data: {
       org: role.org,
       name: role.name,
-      type: role.type,
+      type: role.type ? roleTypes[role.type] : '',
       epics: role.epics.map((epic) => epic.name).join(', '),
       period: role.period,
       link: { dest: role.org.url, label: `Homepage for ${role.org.name}`, variant: 'plain' },
