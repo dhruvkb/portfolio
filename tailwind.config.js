ab47f8f9232fafe0f629ba5a56e8e779f4d6abd7
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-08 12:22:59 +0400

    Add new favicon consistent with other personal projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
-      // `screens` should be synchronised with `@/models/breakpoints.ts`.
       spacing: {
         ch: '1ch',
         indent: '2ch',
