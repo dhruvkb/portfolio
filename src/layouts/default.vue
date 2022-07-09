51abedb6eb9e56907909006c1301126138dc641a
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-10 00:47:09 +0400

    Hoist résumé initialisation up into the layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  import { usePage } from 'iles'
+
+  import { useResume } from '@/stores/resume'
+
+  const { site } = usePage()
+  const resumeStore = useResume()
+  await resumeStore.initResume(site.resumeUrl)
+</script>
+
+<template>
+  <!-- Page content goes here -->
+  <slot />
+</template>
