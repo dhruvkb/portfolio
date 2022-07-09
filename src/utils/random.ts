24cc472ff14e969a9985c7a51b8e23b41a92dbd8
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-10 00:40:57 +0400

    Extract random sampling into a separate util

---
+++ src/utils/random.ts
@@ -0,0 +1,10 @@
+/**
+ * Return a random element from an array.
+ *
+ * @param list - the array from which to choose an element randomly
+ * @return the randomly chosen element
+ */
+export const sample = <T, >(list: T[]): T => {
+  const { length } = list
+  return list[Math.floor(length * Math.random())]
+}
