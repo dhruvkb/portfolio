07a992b99a2735226dd19c12f51f1d653cd0f6a6
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-10 00:45:25 +0400

    Extract square bracket numbers into their own component for reuse

---
+++ src/components/ReferenceIndex.vue
@@ -0,0 +1,16 @@
+<!--
+Renders a de-emphasised number enclosed in square brackets.
+-->
+
+<script setup lang="ts">
+  interface Props {
+    index: number
+  }
+  defineProps<Props>()
+</script>
+
+<template>
+  <span
+    class="font-mono text-sm font-semibold text-neutral-400 dark:text-neutral-600"
+    aria-hidden="true">[{{ index }}]</span>
+</template>
