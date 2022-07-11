f248ca613e1a9d4bd047480484e98e9f4c41032a
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-11 13:01:51 +0400

    Make site title to inherit font size

--- src/components/SiteTitle.vue
+++ src/components/SiteTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="gradient-text from-red-500 to-fuchsia-500 text-3xl leading-snug text-tx selection:bg-neutral-200 selection:text-red-500 dark:selection:bg-neutral-900 sm:text-4xl">
+  <span class="gradient-text from-red-500 to-fuchsia-500 text-tx selection:bg-neutral-200 selection:text-red-500 dark:selection:bg-neutral-900 print:text-red-500">
     <span class="font-black">dhruv</span>
     <span>kb</span>
   </span>
