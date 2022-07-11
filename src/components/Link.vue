e65be87ba8e3adcb11b17f8c2188a1e77434b011
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-11 12:54:12 +0400

    Add support for nav links

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,10 +1,11 @@
 <!--
-Renders a link as a simple HTML anchor tag `<a>` or a `RouterLink` based on
+Renders a link as a simple HTML anchor tag `<a>` with attributes based on
 whether it points to an external resource or a router path. External links open
 in a new tab without a referrer.
 -->
 
 <script setup lang="ts">
+  import { usePage } from 'iles'
   import { computed } from 'vue'
 
   interface Props {
@@ -17,12 +18,18 @@ in a new tab without a referrer.
      * - base: no style
      */
     variant?: 'regular' | 'plain' | 'base'
+    title?: string
   }
   const props = withDefaults(defineProps<Props>(), {
     variant: 'regular',
+    title: undefined,
   })
 
   const isExternal = computed(() => props.dest.startsWith('http'))
+
+  const { frontmatter } = usePage()
+  const isNav = computed(() => props.title !== undefined)
+  const isActive = computed(() => frontmatter.title === props.title)
 
   const params = computed(() => (isExternal.value
     ? { target: '_blank', rel: 'noreferrer' }
@@ -33,7 +40,10 @@ in a new tab without a referrer.
   <div class="group inline-flex flex-row items-center gap-1">
     <a
       class="hover:underline"
-      :class="{ 'text-xs font-semibold uppercase': variant === 'regular' }"
+      :class="{
+        'text-xs font-semibold uppercase': variant === 'regular',
+        'text-neutral-400 dark:text-neutral-600 underline hover:text-curr': isNav && isActive,
+      }"
       v-bind="params"
       :href="dest"
       :aria-label="label">
