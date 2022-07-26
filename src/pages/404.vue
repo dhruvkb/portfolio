07ef616bdaa75d52debe61f8fec5716166be7ab9
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-26 15:12:58 +0400

    Make page structure consistent with other pages

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -9,7 +9,10 @@
 
 <template>
   <main class="px-page py-page flex flex-col gap-6">
-    <div class="relative z-10">
+    <section aria-label="Error">
+      <h2 aria-label="404 Not Found">
+        404 Not Found
+      </h2>
       <p class="body-text mb-6">
         There's nothing here. You seem <strong>lost</strong>.
       </p>
@@ -18,7 +21,7 @@
         dest="/">
         Home
       </Link>
-    </div>
+    </section>
 
     <div class="relative grow">
       <GameOfLife
