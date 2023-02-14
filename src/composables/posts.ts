6fd37e03edaa45fb1a369aaf3cda41b777f09185
Dhruv Bhanushali <dhruv_b@live.com>
2023-02-15 01:08:26 +0400

    Add support for post series

--- src/composables/posts.ts
+++ src/composables/posts.ts
@@ -1,10 +1,15 @@
-import { useDocuments, type PageComponent } from 'iles'
+import { useDocuments, usePage, type PageComponent, type Document } from 'iles'
 import { computed } from 'vue'
 
 export interface Post extends PageComponent {
   title: string
   date: Date
   isFeatured: boolean
+  /**
+   * the name of a post series; Posts with the same series name are listed as
+   * related to each other.
+   */
+  series?: string
   /**
    * the serial index of the post; This is deduced from the first 4 digits in
    * the file name and injected automatically.
@@ -25,5 +30,39 @@ export const usePosts = () => {
     allPostDocs,
     sortedPostDocs,
     featuredPostDocs,
+  }
+}
+
+export const useCurrPost = () => {
+  const frontmatter = usePage().frontmatter as Post
+  const { allPostDocs } = usePosts()
+
+  const currIndex = computed(() => frontmatter.index)
+
+  const adjacentPosts = computed(() => {
+    const adjacent: { sNo: number | string; doc: Document<Post> }[] = []
+    Object.entries({ prev: -1, next: 1 }).forEach(([sNo, offset]) => {
+      const doc = allPostDocs.value.at(currIndex.value + offset)
+      if (doc) adjacent.push({ sNo, doc })
+    })
+    return adjacent
+  })
+
+  const seriesName = computed(() => frontmatter.series)
+  const seriesPosts = computed(() =>
+    seriesName.value
+      ? allPostDocs.value
+          .filter((item) => item.series === seriesName.value)
+          .map((doc, index) => ({ sNo: index + 1, doc }))
+      : []
+  )
+
+  return {
+    currIndex,
+
+    adjacentPosts,
+
+    seriesName,
+    seriesPosts,
   }
 }
