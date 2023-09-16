92ac652c79c6f23f825ec9d310c5178a10f57513
Dhruv Bhanushali <dhruv_b@live.com>
2023-09-16 16:24:33 +0400

    Refine the post page with modular components and simplified styling

--- src/layouts/post.vue
+++ src/layouts/post.vue
@@ -3,82 +3,38 @@ Renders a single post. This layout nests inside the default layout.
 -->
 
 <script setup lang="ts">
-  import { usePage } from 'iles'
-  import { computed } from 'vue'
+  import PostEnjoyedSection from '@/sections/PostEnjoyedSection.vue'
+  import PostHeaderSection from '@/sections/PostHeaderSection.vue'
+  import PostMoreSection from '@/sections/PostMoreSection.vue'
 
-  import { usePosts, type Post } from '@/composables/posts'
-  import type { Arrow } from '@/constants/arrows'
-  import { readableDate } from '@/utils/date'
-
-  const frontmatter = usePage().frontmatter as Post
-
-  const { allPostDocs } = usePosts()
-  const furtherLinks = computed(() => {
-    const { index } = frontmatter
-    const allPosts = allPostDocs.value
-    return [
-      {
-        name: 'Previous',
-        doc: allPosts[index - 1],
-        attrs: { arrow: 'WEST' as Arrow },
-      },
-      {
-        name: 'Next',
-        doc: allPosts[index + 1],
-        attrs: { class: ['ml-auto'] },
-      },
-    ].filter((link) => link.doc)
-  })
+  const symbols = {
+    dashes: '-',
+    equals: '=',
+    tildes: '~',
+  }
+  const styles = Object.fromEntries(
+    Object.entries(symbols).map(([key, value]) => [
+      `--${key}`,
+      `'${value.repeat(88)}'`,
+    ])
+  )
 </script>
 
 <template layout="default">
-  <!-- The `.blog` class enables blog-specific styling. -->
-  <main class="blog mx-auto w-full max-w-text">
-    <aside class="mb-para">
-      <Link
-        dest="/blog"
-        arrow="WEST"
-        class="mb-para block"
-        label="Go to all posts"
-        >All posts</Link
-      >
-    </aside>
+  <main class="blog-main mx-auto w-full max-w-text">
+    <PostHeaderSection class="mb-para" />
 
-    <article class="mb-para">
-      <div class="flex flex-row items-center">
-        <!-- Title -->
-        <h1>{{ frontmatter.title }}</h1>
-
-        <!-- Date of publishing -->
-        <div class="ml-auto">
-          <span class="text-low">Date:</span>
-          {{ readableDate(frontmatter.date) }}
-        </div>
-      </div>
-
-      <div
-        class="text-low"
-        aria-hidden="true">
-        {{ '='.repeat(frontmatter.title.length) }}
-      </div>
-
-      <ContentsTable class="mb-para" />
-
+    <article
+      class="blog-content mb-para"
+      :style="styles">
       <slot />
     </article>
 
-    <div class="flex flex-row">
-      <Link
-        v-for="(link, linkIndex) in furtherLinks"
-        :key="linkIndex"
-        :dest="link.doc.href"
-        v-bind="link.attrs"
-        :label="`Read '${link.doc.title}'`"
-        :is-lowercase="false">
-        <span class="lowercase text-low">{{ link.name }}:</span>
-        {{ link.doc.title }}
-      </Link>
-    </div>
+    <PostEnjoyedSection class="mb-para" />
+    <PostMoreSection
+      class="mb-para"
+      shows-series />
+    <PostMoreSection />
   </main>
 </template>
 
