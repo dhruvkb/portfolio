de616c15ea305f322bea6317e3a16ac04a6e7be0
Dhruv Bhanushali <dhruv_b@live.com>
2024-10-14 14:40:38 +0400

    Add utilities for generating OG images

---
+++ packages/website/src/utils/hash.ts
@@ -0,0 +1,14 @@
+/**
+ * Calculate a hash for a string between 0 and `len`.
+ *
+ * @param text the string to hash
+ * @param len the maximum value of the hash (exclusive)
+ * @returns the hash as a number
+ */
+export function getHash(text: string, len: number): number {
+  let hash = 0
+  for (let i = 0; i < text.length; i++) {
+    hash = (hash * 31 + text.charCodeAt(i)) >>> 0
+  }
+  return hash % len
+}
---
+++ packages/website/src/utils/og_image.ts
@@ -0,0 +1,119 @@
+import fs from 'fs'
+import path from 'path'
+
+import { ImageResponse } from '@vercel/og'
+import { site } from '@/stores/site'
+import { getHash } from '@/utils/hash'
+
+export const COLOR_TEXT_ACCENT = '#fab387'
+
+const fonts = (
+  [
+    { family: 'Inter', variant: 'Regular', weight: 400 },
+    { family: 'Inter', variant: 'Bold', weight: 700 },
+  ] as const
+).map(({ family, variant, weight }) => ({
+  name: family,
+  weight,
+  style: 'normal' as const,
+  data: fs.readFileSync(
+    path.resolve(`./src/assets/fonts/${family}-${variant}.ttf`)
+  ).buffer,
+}))
+
+export interface Element {
+  type: 'div'
+  props: {
+    tw?: string
+    style?: { [key: string]: string | number }
+    children: string | Element | Element[]
+  }
+}
+
+export interface Slots {
+  main: Element
+  right?: Element
+}
+
+/**
+ * Get one of the theme's accent colors to style the heading content.
+ * A hash of the text is used to determine the color so that it is both
+ * pseudo-random and deterministic.
+ *
+ * @param text the text to hash
+ * @returns the accent color
+ */
+export function getAccentColor(text: string): string {
+  const accentColors = [
+    '#f5e0dc', // rosewater
+    '#f2cdcd', // flamingo
+    '#f5c2e7', // pink
+    '#cba6f7', // mauve
+    '#f38ba8', // red
+    '#eba0ac', // maroon
+    '#fab387', // peach
+    '#f9e2af', // yellow
+    '#a6e3a1', // green
+    '#94e2d5', // teal
+    '#89dceb', // sky
+    '#74c7ec', // sapphire
+    '#89b4fa', // blue
+    '#b4befe', // lavender
+  ]
+  return accentColors[getHash(text, accentColors.length)] ?? '#f5e0dc' // rosewater
+}
+
+/**
+ * Create an image response with the site's title and custom slots.
+ * The slots are used to customize the content of the image.
+ *
+ * @param slots the slots to customize the image
+ * @returns the image response
+ */
+export function getImage(slots: Slots): ImageResponse {
+  return new ImageResponse(
+    {
+      type: 'div',
+      props: {
+        tw: 'w-full h-full flex',
+        style: {
+          backgroundImage: 'linear-gradient(to bottom, #1e1e2e, #181825)',
+          color: '#cdd6f4',
+          backgroundSize: '1200px 686px',
+          fontFamily: 'Inter',
+        },
+        children: {
+          type: 'div',
+          props: {
+            tw: 'h-full w-full flex flex-col justify-between p-16',
+            children: [
+              {
+                type: 'div',
+                props: {
+                  tw: 'mb-10 flex flex-row justify-between',
+                  style: { color: '#7f849c' },
+                  children: [
+                    {
+                      type: 'div',
+                      props: {
+                        tw: 'font-bold text-3xl',
+                        children: site.title,
+                      },
+                    },
+                    slots.right ?? { type: 'div', props: {} },
+                  ],
+                },
+              },
+              slots.main,
+            ],
+          },
+        },
+      },
+    },
+    {
+      width: 1200,
+      height: 600,
+      fonts,
+    }
+  )
+}
