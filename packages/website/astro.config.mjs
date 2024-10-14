984e005c07f156c5f463f55c8d809b5ef3499912
Dhruv Bhanushali <dhruv_b@live.com>
2024-10-14 14:40:03 +0400

    Scaffold Astro

---
+++ packages/website/astro.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig, passthroughImageService } from 'astro/config'
+
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+
+import { remarkMtime } from './src/plugins/remark_mtime'
+import { rehypeTailwind } from './src/plugins/rehype_tailwind'
+import { watchPlugins } from './src/integrations/watch_plugins'
+
+import site from './src/data/site.json'
+import flexokiDark from './src/assets/themes/flexoki-dark.json'
+
+// https://astro.build/config
+export default defineConfig({
+  site: site.baseUrl,
+  image: {
+    service: passthroughImageService(),
+  },
+  markdown: {
+    remarkPlugins: [remarkMtime],
+    rehypePlugins: [rehypeTailwind],
+    smartypants: false, // https://daringfireball.net/projects/smartypants/
+    shikiConfig: {
+      defaultColor: false,
+      themes:  {
+        dark: 'catppuccin-mocha',
+        light: 'catppuccin-latte',
+      }
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [tailwind({ applyBaseStyles: false }), mdx(), watchPlugins()],
+})
