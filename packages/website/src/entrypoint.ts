ac61ca99c219986113f7c098849cca9b8836ee00
Dhruv Bhanushali <dhruv_b@live.com>
2024-10-24 11:30:42 +0400

    Create Alpine store for the writings page

--- packages/website/src/entrypoint.ts
+++ packages/website/src/entrypoint.ts
@@ -1,4 +1,24 @@
 import type { Alpine } from 'alpinejs'
 
+export interface WritingsStore {
+  activeCategory: string | undefined
+  setActiveCategory(category: string): void
+}
+
 export default (Alpine: Alpine) => {
+  Alpine.store('writings', {
+    activeCategory: undefined,
+
+    /**
+     * Set the given category as the active category. If the category is already
+     * active, clear it.
+     */
+    setActiveCategory(category: string) {
+      if (this.activeCategory === category) {
+        this.activeCategory = undefined
+      } else {
+        this.activeCategory = category
+      }
+    },
+  } satisfies WritingsStore)
 }
