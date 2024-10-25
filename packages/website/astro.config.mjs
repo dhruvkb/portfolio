f095baaedea65adc063a342305cf38b3d0eaa0a0
Dhruv Bhanushali <dhruv_b@live.com>
2024-10-25 14:59:06 +0400

    Fix missing comma

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -35,6 +35,6 @@ export default defineConfig({
     tailwind({ applyBaseStyles: false }),
     mdx(),
     watchPlugins(),
-    alpinejs({ entrypoint: '/src/entrypoint' })
+    alpinejs({ entrypoint: '/src/entrypoint' }),
   ],
 })
