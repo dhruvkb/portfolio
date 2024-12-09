4e29ff136f1df26ee493e8b8279e5a4f66e1edb3
Dhruv Bhanushali <dhruv_b@live.com>
2024-12-09 19:36:45 +0400

    Replace the global store with `x-data`

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -33,6 +33,6 @@ export default defineConfig({
     tailwind({ applyBaseStyles: false }),
     mdx(),
     watchPlugins(),
-    alpinejs({ entrypoint: '/src/entrypoint' }),
+    alpinejs(),
   ],
 })
