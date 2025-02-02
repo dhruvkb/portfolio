4cb4d4199f34f45222147187bc7174db96c76605
Dhruv Bhanushali <dhruv_b@live.com>
2025-02-02 07:02:05 +0530

    Fix types for `window`/`globalThis`

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
 
-interface Window {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-  Alpine: import('alpinejs').Alpine
-}
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+declare const Alpine: import('alpinejs').Alpine
